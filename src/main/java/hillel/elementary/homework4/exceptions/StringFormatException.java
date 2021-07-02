eb933517aa30f30794214068da46f48f0207c70a
Helen Sotnik <politovaoo@gmail.com>
2021-07-02 20:26:25 +0300

    Homework 4 - String methods and Regular expressions practicing.

---
+++ src/main/java/hillel/elementary/homework4/exceptions/StringFormatException.java
@@ -0,0 +1,7 @@
+package hillel.elementary.homework4.exceptions;
+
+public class StringFormatException extends Exception{
+    public StringFormatException(String message) {
+        super(message);
+    }
+}
