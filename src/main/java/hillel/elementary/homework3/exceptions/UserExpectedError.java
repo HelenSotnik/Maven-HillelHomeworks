339da163154338eda612383a05a5527d65c2f0cd
Helen Sotnik <politovaoo@gmail.com>
2021-07-02 20:19:02 +0300

    Homework 3 - Checked/Unchecked exceptions.

---
+++ src/main/java/hillel/elementary/homework3/exceptions/UserExpectedError.java
@@ -0,0 +1,7 @@
+package hillel.elementary.homework3.exceptions;
+
+public class UserExpectedError extends RuntimeException {
+    public UserExpectedError(String message) {
+        super(message);
+    }
+}
