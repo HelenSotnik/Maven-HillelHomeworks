019e56fc74cf7b930cd6323c18eb4d0410ec3667
Helen Sotnik <politovaoo@gmail.com>
2021-09-02 00:14:06 +0300

    Changes in ThreadTest

--- src/main/java/hillel/elementary/homework18/thread/ThreadTest.java
+++ src/main/java/hillel/elementary/homework18/thread/ThreadTest.java
@@ -3,26 +3,25 @@ package hillel.elementary.homework18.thread;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadTest {
-    private static AtomicInteger atomicInteger = new AtomicInteger(50000);
+    private static AtomicInteger atomicInteger = new AtomicInteger(40000);
 
     public static void main(String[] args) throws InterruptedException {
         Thread myThread1 = new Thread(ThreadTest::method);
-        myThread1.setName("Thread1");
+        myThread1.start();
 
         Thread myThread2 = new Thread(ThreadTest::method);
-        myThread2.setName("MyThread2");
-
-        myThread1.start();
         myThread2.start();
 
-        Thread.sleep(1000);
-        System.out.println("atomic integer: " + atomicInteger.get());
+        Thread.sleep(2000);
+        System.out.println("ATOMIC INTEGER: " + atomicInteger.get());
     }
 
-    private static synchronized void method() {
+    private static void method() {
         for (int i = 10000; i > 0; i--) {
-            System.out.println(Thread.currentThread().getName() + " " + i);
-            atomicInteger.decrementAndGet();
+            Thread thread = new Thread(() -> {
+                atomicInteger.decrementAndGet();
+            });
+            thread.start();
         }
     }
 }
